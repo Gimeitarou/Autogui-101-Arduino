3c72568ebf4b6ff623f96bc0625e188eca54942d
Gimeitarou <300kyen@gmail.com>
2024-12-23 13:33:21 +0900

    Update Autogui-101-Arduino.ino

--- Autogui-101-Arduino.ino
+++ Autogui-101-Arduino.ino
@@ -3,10 +3,10 @@
 #include <Keyboard.h>
 
 //charDef
-char winkey = 'KEY_LEFT_GUI'
-char ctrl = 'KEY_LEFT_CTRL'
-char DownloadCmd = 'powershell Invoke-WebRequest -Uri "www.python.jp/pages/python_logo2.png" -OutFile "~/Downloads/TheHopeOne.png"'
-char OpenCmd = 'C:/Users/%username%/Downloads/TheHopeOne.png'
+char winkey = 'KEY_LEFT_GUI';
+char ctrl = 'KEY_LEFT_CTRL';
+char DownloadCmd = 'powershell Invoke-WebRequest -Uri "www.python.jp/pages/python_logo2.png" -OutFile "~/Downloads/TheHopeOne.png"';
+char OpenCmd = 'C:/Users/%username%/Downloads/TheHopeOne.png';
 
 void setup(){
   Keyboard.begin();
@@ -19,23 +19,23 @@ void loop(){
   Keyboard.press('r');
   delay(300);//0.3sec,hotkeyTime
   Keyboard.releaseAll();
-  Keyboard.printIn(DownloadCmd)
-  dalay(3000)//3sec,waitForDownload
+  Keyboard.println(DownloadCmd);
+  delay(3000);//3sec,waitForDownload
 
   //open the pic
   Keyboard.press(winkey);
   Keyboard.press('r');
   delay(300);//0.3sec//0.3sec,hotkeyTime
   Keyboard.releaseAll();
-  Keyboard.printIn(OpenCmd)
-  delay(1000)//1sec,waitForOpeningPic
+  Keyboard.println(OpenCmd);
+  delay(1000);//1sec,waitForOpeningPic
 
   //set it as wallpaper
   Keyboard.press(ctrl);
   Keyboard.press('b');
   delay(300);//0.3sec//0.3sec,hotkeyTime
   Keyboard.releaseAll();
-  delay(1000)//1sec,waitForSettingWallpaper
+  delay(1000);//1sec,waitForSettingWallpaper
 
   //close the pic
   Keyboard.press(KEY_LEFT_ALT);
