c44b6c4b79bbbc37749da781d9afcaa63c9bc67a
Gimeitarou <300kyen@gmail.com>
2025-03-22 16:09:50 +0900

    Update Autogui-101-Arduino.ino

--- Autogui-101-Arduino.ino
+++ Autogui-101-Arduino.ino
@@ -1,33 +1,36 @@
-//refference to https://speeddemon.jp/keyboard-h/#toc12
+//Copyright (c) 2024 Gimeitarou
+//This software is released under the MIT License, see LICENSE.
+
+//learned from https://speeddemon.jp/keyboard-h/#toc12
 
 #include <Keyboard.h>
 
 void setup(){
   Keyboard.begin();
-  delay(3000);//3sec
+  delay(2000);//2sec
 
-//download a pic on DL-dir
+  //download a pic on DL-dir
   Keyboard.press(KEY_LEFT_GUI);
   Keyboard.press('r');
-  delay(500);//0.5sec,hotkeyTime
+  delay(100);//0.1sec,hotkeyTime
   Keyboard.releaseAll();
   Keyboard.print("powershell Invoke-WebRequest -Uri gahag.net/img/201608/26s/gahag-0119154009-1.jpg -OutFile Downloads/Lake.jpg\n");
-  delay(7000);//7sec,waitForDownload
+  delay(10000);//10sec,waitForDownload
 
   //open the pic
   Keyboard.press(KEY_LEFT_GUI);
   Keyboard.press('r');
-  delay(500);//0.5sec,hotkeyTime
+  delay(100);//0.1sec,hotkeyTime
   Keyboard.releaseAll();
   Keyboard.print("C'/Users/%username%/Downloads/Lake.jpg\n");
-  delay(5000);//3sec,waitForOpeningPic
+  delay(2000);//2sec,waitForOpeningPic
 
   //set it as wallpaper
   Keyboard.press(KEY_LEFT_CTRL);
   Keyboard.press('b');
   delay(100);//0.1sec//0.3sec,hotkeyTime
   Keyboard.releaseAll();
-  delay(3000);//3sec,waitForSettingWallpaper
+  delay(2000);//2sec,waitForSettingWallpaper
 
   //close the pic
   Keyboard.press(KEY_LEFT_ALT);
